2e8a7406e283f95b88b47ea56f3782c2128b7230
Vito Corl <elhkadrawiaymane@gmail.com>
2025-09-08 14:48:12 +0100

    program that reads a main character string and a substring, then checks if the substring is present in the main string

---
+++ string/Find_Sub-String.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    char str[] = "ayne is the best :)ma";
+    char str1[] = "ma";
+    for(int i = 0; str[i] != '\0'; i++){
+        if(strstr(str, str1) != NULL){
+            printf("substence founded ! \n");
+            break;
+        }else{
+            printf("substence not founded !\n");
+            break;
+        }
+    }
+}
